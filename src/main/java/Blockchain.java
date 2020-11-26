3f8d8c1710b035676c09e7708190597a24aab7f9
DMYTRO <GorovDimon11>
2020-11-26 20:09:42 +0200

    Added Consensus algorithm

--- src/main/java/Blockchain.java
+++ src/main/java/Blockchain.java
@@ -1,16 +1,86 @@
+import com.google.common.hash.Hashing;
+import com.google.gson.Gson;
 
+import java.io.*;
+import java.net.*;
 import java.security.MessageDigest;
 import java.nio.charset.StandardCharsets;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class Blockchain {
     private List<Block> GDRchain = new ArrayList<>();
     private List<Transaction> GDRcurrentTransactions = new ArrayList<>();
 
+    public Set<String> getGDRnodes() {
+        return GDRnodes;
+    }
+
+    private Set<String> GDRnodes = new HashSet<>();
+
     public Blockchain() {
         GDRnewBlock(11052001, "Gorovenko");
+    }
+
+    public void GDRregisterNode(String netloc) {
+        GDRnodes.add(netloc);
+    }
+
+    public boolean GDRvalidChain(List<Block> chain) {
+        for (int i = 1; i < chain.size(); i++) {
+            Block lastBlock = chain.get(i - 1);
+            Block currentBlock = chain.get(i);
+            if (!currentBlock.getGDRpreviousHash().equals(GDR_hash(lastBlock))) {
+                System.out.println("Hash don't match");
+                return false;
+            }
+            if (!GDRisProofValid(lastBlock.getGDRproof(), currentBlock.getGDRproof())) {
+                System.out.println("Proof is not valid");
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public boolean GDRresolveConflicts() {
+        Gson gson = new Gson();
+        int maxLen = this.GDRchain.size();
+        List<Block> newChain = this.GDRchain;
+        try {
+            for (String host : this.GDRnodes) {
+                URL url;
+                url = new URL(host + "/chain");
+                HttpURLConnection con = (HttpURLConnection) url.openConnection();
+                con.setRequestMethod("GET");
+                int status = con.getResponseCode();
+                if (status == 200) {
+                    BufferedReader in = new
+                            BufferedReader(new InputStreamReader(con.getInputStream()));
+                    String inputLine;
+                    StringBuffer content = new
+                            StringBuffer();
+                    while ((inputLine = in.readLine()) !=
+                            null) {
+                        content.append(inputLine);
+                    }
+                    in.close();
+                    con.disconnect();
+                    ChainResponse response =
+                            gson.fromJson(content.toString(), ChainResponse.class);
+                    if (response.length > maxLen &&
+                            GDRvalidChain(response.chain)) {
+                        maxLen = response.length;
+                        newChain = response.chain;
+                    }
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        if (newChain != this.GDRchain) {
+            this.GDRchain = newChain;
+        }
+        return newChain == this.GDRchain;
     }
 
     /**
@@ -19,8 +89,8 @@ public class Blockchain {
      * заголовних нуля, де p — попередній p є попереднім доказом, а p`
      * — 5новим
      *
-     * @parama lastProofOfWork
      * @return int
+     * //@parama lastProofOfWork
      */
     public int GDRproofOfWork(String prevHash) {
         int GDRproof = 0;
@@ -33,24 +103,23 @@ public class Blockchain {
     /**
      * Підтвердження доказу: Чи містить hash(lastProof, proof)
      * заголовних нуля
-     *
-     * @param lastProof
-     * @param proof
-     * @return
-     *
+     * <p>
+     * /@param lastProof
+     * /@param proof
+     * /@return
      */
-    /*private boolean GDRisProofValid(int lastProof, int proof) {
+    private boolean GDRisProofValid(int lastProof, int proof) {//TODO
 
         String GDRguessString = Integer.toString(lastProof) +
                 Integer.toString(proof);
         String GDRguessHash = Hashing.sha256().hashString(GDRguessString,
                 StandardCharsets.UTF_8).toString();
         return GDRguessHash.endsWith("05");
-    }*/
+    }
 
     /**
+     * /@param proof        Докази проведенної роботи
      *
-     * @param proof Докази проведенної роботи
      * @param previousHash Хеш попереднього блока
      * @return Новий блок
      * 6
@@ -71,15 +140,13 @@ public class Blockchain {
     }
 
     /**
-     *
      * Направляє нову транзакцію в наступний блок
+     * <p>
+     * /@param sender    Адреса відправника
      *
-     *
-     * @param sender Адреса відправника
      * @param recipient Адреса отримувача
-     * @param amount Сума
+     *                  /@param amount    Сума
      * @return Індекса блока, що буде зберігати цю транзакцію
-     *
      */
 
     public int GDRnewTransaction(String sender, String recipient, int amount) {
@@ -89,10 +156,8 @@ public class Blockchain {
     }
 
     /**
-     *
-     * @parama block Блок
      * @return Хеш блока
-     *
+     * /@parama block Блок
      */
     public static String GDRcreateHash(int index, int proof, String prevHash) {
         StringBuilder GDRhashingInputBuilder = new StringBuilder();
@@ -128,19 +193,19 @@ public class Blockchain {
     }
 
     public static String sha256(String base) {
-        try{
+        try {
             MessageDigest digest = MessageDigest.getInstance("SHA-256");
             byte[] hash = digest.digest(base.getBytes("UTF-8"));
             StringBuffer hexString = new StringBuffer();
 
             for (int i = 0; i < hash.length; i++) {
                 String hex = Integer.toHexString(0xff & hash[i]);
-                if(hex.length() == 1) hexString.append('0');
+                if (hex.length() == 1) hexString.append('0');
                 hexString.append(hex);
             }
 
             return hexString.toString();
-        } catch(Exception ex){
+        } catch (Exception ex) {
             throw new RuntimeException(ex);
         }
     }
--- src/main/java/Controller.java
+++ src/main/java/Controller.java
@@ -2,6 +2,7 @@ import com.google.gson.Gson;
 import com.google.gson.JsonSyntaxException;
 import spark.Spark;
 
+import java.util.List;
 import java.util.UUID;
 
 import static spark.Spark.*;
@@ -19,39 +20,65 @@ public class Controller {
             int proofOfWork = blockchain.GDRproofOfWork(lastProof);
 
             blockchain.GDRnewTransaction("0",
-                UUID.randomUUID().toString().replace("-", ""), 11);
+                    UUID.randomUUID().toString().replace("-", ""), 11);
             String lastHash = Blockchain.GDR_hash(lastBlock);
             Block newBlock = blockchain.GDRnewBlock(blockchain.GDRproofOfWork(lastHash), lastHash);
             //Block newBlock = blockchain.GDRnewBlock(proofOfWork, lastHash);
-        // створюємо рядок, що є відображенням нового блока
+            // створюємо рядок, що є відображенням нового блока
             String json = gson.toJson(newBlock);
-        // ставимо статус відповіді 200
+            // ставимо статус відповіді 200
             res.status(200);
-        // відправляюємо відомості про новий блок
+            // відправляюємо відомості про новий блок
             return json;
         });
 
-    post("/transactions/new", (req, res) -> {
-        try {
-             Transaction transaction = gson.fromJson(req.body(),
-                    Transaction.class);
-             // додаємо нову транзакцію
-             int index =blockchain.GDRnewTransaction(transaction.getGDRsender(), transaction.getGDRrecipient(),transaction.getGDRamount());
-             // якщо помилок не відбулося, то відправляємо відповідь
-             res.status(201);
-             return "Transaction will be added to Block " + index;
-        } catch (JsonSyntaxException e) {
-            // якщо запит має неправильну структуру - повертаємо по-милку
-            res.status(400);
-            return "Invalid JSON";
-        }
+        post("/transactions/new", (req, res) -> {
+            try {
+                Transaction transaction = gson.fromJson(req.body(),
+                        Transaction.class);
+                // додаємо нову транзакцію
+                int index = blockchain.GDRnewTransaction(transaction.getGDRsender(), transaction.getGDRrecipient(), transaction.getGDRamount());
+                // якщо помилок не відбулося, то відправляємо відповідь
+                res.status(201);
+                return "Transaction will be added to Block " + index;
+            } catch (JsonSyntaxException e) {
+                // якщо запит має неправильну структуру - повертаємо по-милку
+                res.status(400);
+                return "Invalid JSON";
+            }
 
-    });
+        });
 
-    get("/chain", (req, res) -> {
-        return gson.toJson(blockchain.GDRgetChain());
-    });
+        get("/chain", (req, res) -> {
+            return gson.toJson(blockchain.GDRgetChain());
+        });
 
-}
+        post("/nodes/register", (req, res) -> {
+            try {
+                List<String> nodes = gson.fromJson(req.body(),
+                        NodesResponse.class).nodes;
+                for (String node : nodes) {
+                    blockchain.GDRregisterNode(node);
+                }
+                return gson.toJson(blockchain.getGDRnodes());
+            } catch (Exception e) {
+                res.status(400);
+                return "Incorrect host address";
+            }
+        });
+
+        get("/nodes/resolve", (req, res) -> {
+            if (blockchain.GDRresolveConflicts()) {
+                return gson.toJson(new
+                        ChainResponse(blockchain.GDRgetChain(), blockchain.GDRgetChain().size()));
+            } else {
+                return gson.toJson(new
+                        ChainResponse(blockchain.GDRgetChain(), blockchain.GDRgetChain().size()));
+            }
+
+        });
+
+
+    }
 
 }
